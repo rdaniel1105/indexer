d17695efe9623b002ec6dfc318ef0b801a977418
Ricardo Ruiz <rgomez051999@gmail.com>
2022-11-11 15:44:50 -0600

    concurrency implemented and function to avoid duplicity has been added.

--- helpers/ndjson_formatter.go
+++ helpers/ndjson_formatter.go
@@ -8,44 +8,42 @@ import (
 // WriteEmailInNDJSON writes the email with the ndjson format to the file
 func WriteEmailInNDJSON(fullEmail models.Email) string {
 	jsonEmail, _ := json.Marshal(fullEmail)
+	data := string(jsonEmail)
+
 	// myDirectory, err := os.Getwd()
 	// fullDirectory := myDirectory + "/emails1.ndjson"
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
+
 	// if _, err := os.Stat(fullDirectory); err == nil {
-	// 	// path/to/whatever exists
 	// 	file, err := os.OpenFile(fullDirectory, os.O_APPEND|os.O_WRONLY, 0644)
 	// 	if err != nil {
 	// 		fmt.Println(err)
 	// 	}
-	// 	data := "\n" + `{ "index" : { "_index" : "myindex" } }` + "\n" + string(jsonEmail)
 
 	// 	defer file.Close()
 
-	// 	_, err2 := file.WriteString(data)
-	// 	if err2 != nil {
-	// 		log.Fatal(err2)
+	// 	_, err = file.WriteString(data)
+	// 	if err != nil {
+	// 		log.Fatal(err)
 	// 	}
 	// 	//BulkData(data)
 	// 	return "Done!"
 	// }
 
-	// file, err1 := os.Create("emails1.ndjson")
-
-	// if err1 != nil {
-	// 	log.Fatal(err1)
+	// file, err := os.Create("emails1.ndjson")
+	// if err != nil {
+	// 	log.Fatal(err)
 	// }
-	// data := `{ "index" : { "_index" : "myindex" } }` + "\n" + string(jsonEmail)
 
 	// defer file.Close()
 
-	// _, err2 := file.WriteString(data)
+	// _, err = file.WriteString(data)
 
-	// if err2 != nil {
-	// 	log.Fatal(err2)
+	// if err != nil {
+	// 	log.Fatal(err)
 	// }
-	data := `{ "index" : { "_index" : "myindex" } }` + "\n" + string(jsonEmail)
 
 	return data
 }
