0ec18ff7c3b71cab4d8e96776a4f39feb58ba926
Ricardo Ruiz <rgomez051999@gmail.com>
2022-11-12 03:31:18 -0600

    file creation has been removed from the program

--- helpers/ndjson_formatter.go
+++ helpers/ndjson_formatter.go
@@ -3,9 +3,6 @@ package helpers
 import (
 	"encoding/json"
 	"example/indexer/models"
-	"fmt"
-	"log"
-	"os"
 )
 
 // WriteEmailInNDJSON writes the email with the ndjson format to the file
@@ -13,48 +10,7 @@ func WriteEmailInNDJSON(fullEmail models.Email) string {
 	jsonEmail, _ := json.Marshal(fullEmail)
 	data := string(jsonEmail)
 
-	myDirectory, err := os.Getwd()
-	filePath := myDirectory + "/emails.ndjson"
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if _, err := os.Stat(filePath); err == nil {
-		file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer fileClose(file)
-
-		data = "\n" + data
-
-		_, err = file.WriteString(data)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return data
-	}
-
-	file, err := os.Create("emails1.ndjson")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer fileClose(file)
-
-	_, err = file.WriteString(data)
-	if err != nil {
-		log.Fatal(err)
-	}
+	data = "\n" + data
 
 	return data
-}
-
-func fileClose(file *os.File) {
-	err := file.Close()
-	if err != nil {
-		fmt.Println(errCloseResBody, err)
-	}
 }
